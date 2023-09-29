5bf44afeae713ebb1202e51a47c454a77f74d054
Jon Craton <jncraton@gmail.com>
2023-09-29 11:41:35 -0400

    Add handout starter code

---
+++ guess.py
@@ -0,0 +1,12 @@
+import random
+
+# Select the game mode
+print("Would you like to:")
+print("1) Guess my number")
+print("2) Pick a number for me to guess")
+
+game_mode = input("Enter selection:")
+
+# Handle mode 1
+
+# Handle mode 2
